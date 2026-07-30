8ead1be2eabd6fb422b2a55900230cc5c72c7fe8
Ghayathri3110 <ghayathridevigm.2006@gmail.com>
2026-07-30 19:25:03 +0530

    removed a testing file

--- summa.py
+++
@@ -1,4 +0,0 @@
-from retrieval_agent import retrieve_context
-print(retrieve_context("computer science", top_k=3))
-print(retrieve_context("mathematics", top_k=3))
-print(retrieve_context("history", top_k=3))
